cc187156da8efbe0c48803917485394e7258fd3f
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2024-09-22 23:20:52 +0530

    feat: Add key mappings for window movement and splitting

--- lua/devinda/remap.lua
+++ lua/devinda/remap.lua
@@ -1,17 +1,10 @@
 vim.g.mapleader = " "
 
 vim.keymap.set({ "n", "v" }, "cy", '"+y', { desc = "Copy to System Clipboard" })
-vim.keymap.set("n", "dW", function()
-	local line = vim.api.nvim_get_current_line()
-	local col = vim.api.nvim_win_get_cursor(0)[2]
-	local after_cursor = line:sub(col + 1)
-	local quote_pos = after_cursor:find('"')
 
-	if quote_pos then
-		-- If there's a quote after the cursor, delete up to it
-		vim.cmd('normal! vt"d')
-	else
-		-- Otherwise, perform the standard dW action
-		vim.cmd("normal! dW")
-	end
-end, { noremap = true, silent = true })
+vim.keymap.set({ "n" }, "<M-e>", "<C-w>h", { desc = "Move window left" })
+vim.keymap.set({ "n" }, "<M-j>", "<C-w>j", { desc = "Move window bottom" })
+vim.keymap.set({ "n" }, "<M-k>", "<C-w>k", { desc = "Move window top" })
+vim.keymap.set({ "n" }, "<M-l>", "<C-w>l", { desc = "Move window right" })
+vim.keymap.set({ "n" }, "<M-v>", "<C-w>v", { desc = "Split Vertically" })
+vim.keymap.set({ "n" }, "<M-q>", "<C-w>q", { desc = "Quit Window" })
