4778622ead6c7e5e2082e978581bf19e65634129
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2024-09-26 00:33:24 +0530

    style: Update key mapping for moving window left

--- lua/devinda/remap.lua
+++ lua/devinda/remap.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 vim.keymap.set({ "n", "v" }, "cy", '"+y', { desc = "Copy to System Clipboard" })
 
-vim.keymap.set({ "n" }, "<M-e>", "<C-w>h", { desc = "Move window left" })
+vim.keymap.set({ "n" }, "<M-h>", "<C-w>h", { desc = "Move window left" })
 vim.keymap.set({ "n" }, "<M-j>", "<C-w>j", { desc = "Move window bottom" })
 vim.keymap.set({ "n" }, "<M-k>", "<C-w>k", { desc = "Move window top" })
 vim.keymap.set({ "n" }, "<M-l>", "<C-w>l", { desc = "Move window right" })
